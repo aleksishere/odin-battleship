29268aaf02ddcd63ab13ba343c91fa47964dcbd6
Aleks Michalak <24192100+aleksishere@users.noreply.github.com>
2024-06-17 23:51:25 +0200

    Minor fixes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const boardsDiv = document.getElementById('boards');
+const restartBtn = document.getElementById('restart-btn');
 
 class Ship {
   constructor(length) {
@@ -62,21 +63,43 @@ class Gameboard {
     }
   }
 
-  receiveAttack(x,y) {
+  receiveAttack(x, y) {
+    if (this.board[y][x] == "hit" || this.board[y][x] == "miss") {
+      console.warn("User already clicked on this.")
+      return false;
+    }
     if (this.board[y][x] !== null) {
       const ship = this.board[y][x]
       ship.hit();
-      this.board[y][x] = 'hit';
+      this.board[y][x] = "hit"
+      changeTurn()
       return { result: "hit", sunk: ship.isSunk() };
     } else {
-      this.board[y][x] = 'miss';
+      this.board[y][x] = "miss"
+      changeTurn()
       return { result: 'miss' };
     }
   }
+
+  checkIfPlayerWon() {
+    for (let i = 0; i < this.board.length; i++) {
+        for (let j = 0; j < this.board[i].length; j++) {
+          const cell = this.board[i][j];
+          if (cell instanceof Ship) {
+            if (!cell.isSunk()) {
+              return false;
+            }
+          }
+        }
+      }
+      return true;
+  }
+
 }
 
+
 class Guessboard {
-  constructor(playerName, opponent=null) {
+  constructor(playerName, opponent = null) {
     this.playerName = playerName;
     this.opponent = opponent;
     this.board = [];
@@ -93,27 +116,31 @@ class Guessboard {
     this.opponent = enemy;
   }
 
-  attack(x,y) {
-    return this.opponent.receiveAttack(x,y)
-  }
-
-  guess(x, y, player, opponent) {
-     if (opponent.gameboard.board[y][x] !== null) {
-      player.guessboard.board[y][x] = 'hit';
-      return { result: "hit" };
+  guess(x, y) {
+    if (this.opponent.board[y][x] === "miss") {
+      console.warn("User already clicked on this.")
+      return false;
+    } else if (this.opponent.board[y][x] !== null) {
+      this.board[y][x] = 'hit';
     } else {
-      opponent.guessboard.board[y][x] = 'miss';
-      return { result: 'miss' };
-     }
+      this.board[y][x] = 'miss';
+    }
+    return false;
   }
 }
 
 class Player {
-  constructor(name) {
+  constructor(name="Player") {
     this.name = name;
     this.gameboard = new Gameboard(name);
     this.guessboard = new Guessboard(name);
     this.turn = false;
+  }
+
+  setName(name) {
+    this.name = name
+    this.gameboard = new Gameboard(name);
+    this.guessboard = new Guessboard(name);
   }
 
 }
@@ -130,94 +157,117 @@ class BotPlayer extends Player {
   }
 }
 
-function startGame(p1Name, p2Name) {
-  const player1 = new RealPlayer(p1Name);
-  const player2 = new RealPlayer(p2Name);
-  player1.turn = true;
+const player1 = new RealPlayer();
+const player2 = new RealPlayer();
+player1.setName("Aleks");
+player2.setName("Player 2");
+player1.turn = true;
+player1.guessboard.setOpponent(player2.gameboard)
+player2.guessboard.setOpponent(player1.gameboard)
 
-  player1.guessboard.setOpponent(player2.gameboard)
-  player2.guessboard.setOpponent(player1.gameboard)
-
-
-  player1.gameboard.placeShip(new Ship(3), 0, 0, "vertical");
-  player1.gameboard.placeShip(new Ship(3), 0, 3, "horizontal");
-  player1.gameboard.placeShip(new Ship(3), 7, 0, "vertical");
-  player1.gameboard.placeShip(new Ship(3), 3, 8, "horizontal");
-
-  player2.gameboard.placeShip(new Ship(3), 0, 3, "horizontal");
-  player2.gameboard.placeShip(new Ship(3), 3, 2, "vertical");
-  player2.gameboard.placeShip(new Ship(3), 7, 1, "vertical");
-  player2.gameboard.placeShip(new Ship(3), 4, 7, "horizontal");
-  player1.guessboard.attack(0, 3);
-  drawBoards(player1, player2);
-}
+player1.gameboard.placeShip(new Ship(3), 0, 0, "vertical");
+player1.gameboard.placeShip(new Ship(3), 0, 3, "horizontal");
+player1.gameboard.placeShip(new Ship(3), 7, 0, "vertical");
+player1.gameboard.placeShip(new Ship(3), 3, 8, "horizontal");
+  
+player2.gameboard.placeShip(new Ship(3), 0, 3, "horizontal");
+player2.gameboard.placeShip(new Ship(3), 3, 2, "vertical");
+player2.gameboard.placeShip(new Ship(3), 7, 1, "vertical");
+player2.gameboard.placeShip(new Ship(3), 4, 7, "horizontal");
+drawBoards(player1, player2);
 
 function drawBoards(player1, player2) {
   boardsDiv.innerHTML = '';
-  const players = [player1, player2]
+  const players = [player1, player2];
   players.forEach(player => {
-    const pGameboard = document.createElement('div');
-    const pGuessboard = document.createElement('div');
-    const pNickname = document.createElement('div');
-    pNickname.innerText = player.gameboard.playerName; 
-    pGuessboard.id = player.gameboard.playerName;
-    boardsDiv.appendChild(pGameboard);
-    pGameboard.appendChild(pNickname);
-    boardsDiv.appendChild(pGuessboard);
+    const playerContainer = document.createElement('div');
+    playerContainer.classList.add("player-container");
 
-    player.gameboard.board.forEach(row => {
+    const pNickname = document.createElement('div');
+    pNickname.innerText = player.gameboard.playerName;
+    pNickname.classList.add("nickname");
+    playerContainer.appendChild(pNickname);
+
+     const pGameboard = document.createElement('div');
+      pGameboard.classList.add("gameboard");
+      player.gameboard.board.forEach(row => {
       const rowDiv = document.createElement("div");
-      rowDiv.style = "display: flex;"
-      pGameboard.appendChild(rowDiv)
+      rowDiv.classList.add("row");
+      pGameboard.appendChild(rowDiv);
       row.forEach((cell) => {
         const cellDiv = document.createElement('div');
+        cellDiv.classList.add("cell");
         if (cell === null) {
-            cellDiv.innerText = 'O';
-        } else if (cell === 'hit') {
-            cellDiv.innerText = 'X';
-        } else if (cell === 'miss') {
-            cellDiv.innerText = '*';
+          cellDiv.classList.add('noShip')
+        } else if (cell === "hit") {
+          cellDiv.classList.add('hitShip')
+        } else if (cell === "miss") {
+          cellDiv.classList.add('missShip')
         } else {
-            cellDiv.innerText = 'S';
+          cellDiv.classList.add('ship')
         }
         rowDiv.appendChild(cellDiv);
-      })
-    })
+      });
+    });
+    playerContainer.appendChild(pGameboard);
 
+    const pGuessboard = document.createElement('div');
+    pGuessboard.id = player.gameboard.playerName;
+    pGuessboard.classList.add("guessboard");
     player.guessboard.board.forEach((row, rowIndex) => {
       const rowDiv = document.createElement("div");
-      rowDiv.style = "display: flex;"
-      pGuessboard.appendChild(rowDiv)
+      rowDiv.classList.add("row");
+      pGuessboard.appendChild(rowDiv);
       row.forEach((cell, columnIndex) => {
         const cellDiv = document.createElement('div');
+        cellDiv.classList.add("cell");
+        cellDiv.style.cursor = "pointer";
         cellDiv.addEventListener('click', function (event) {
           if (player1.turn && event.target.parentNode.parentNode.id === player1.name) {
-            player1.guessboard.attack(columnIndex, rowIndex)
-            player1.guessboard.guess(columnIndex, rowIndex, player1, player2)
-            player1.turn = !player1.turn;
-            player2.turn = !player2.turn;
+            player1.guessboard.guess(columnIndex, rowIndex, player1)
+            player2.gameboard.receiveAttack(columnIndex, rowIndex)
           }
           if (player2.turn && event.target.parentNode.parentNode.id === player2.name) {
-            player2.guessboard.attack(columnIndex, rowIndex)
-            player2.guessboard.guess(columnIndex, rowIndex, player2, player1)
-            player1.turn = !player1.turn;
-            player2.turn = !player2.turn;
+            player2.guessboard.guess(columnIndex, rowIndex, player2)
+            player1.gameboard.receiveAttack(columnIndex, rowIndex)
           }
           drawBoards(player1, player2)
-        })
-
+          checkForWin();
+        });
         if (cell === null) {
-            cellDiv.innerText = 'O';
-        } else if (cell === 'hit') {
-            cellDiv.innerText = 'X';
-        } else if (cell === 'miss') {
-            cellDiv.innerText = '*';
+          cellDiv.classList.add('noShip')
+        } else if (cell === "hit") {
+          cellDiv.classList.add('hitShip')
+        } else if (cell === "miss") {
+          cellDiv.classList.add('missShip')
         } else {
-            cellDiv.innerText = 'S';
+          cellDiv.classList.add('ship')
         }
         rowDiv.appendChild(cellDiv);
-      })
-    })
-
-  });
+      });
+    });
+    playerContainer.appendChild(pGuessboard);
+    boardsDiv.appendChild(playerContainer);
+  })
 }
+
+function checkForWin() {
+  if (player1.gameboard.checkIfPlayerWon()) {
+    alert(`${player2.name} won!`);
+  } else if (player2.gameboard.checkIfPlayerWon()) {
+    alert(`${player1.name} won!`);
+  }
+}
+
+function changeTurn() {
+  player1.turn = !player1.turn;
+  player2.turn = !player2.turn;
+}
+
+restartBtn.addEventListener("click", function () {
+  player1.setName(player1.name)
+  player2.setName(player2.name)
+  drawBoards(player1, player2)
+  player1.guessboard.setOpponent(player2.gameboard)
+  player2.guessboard.setOpponent(player1.gameboard)
+})
