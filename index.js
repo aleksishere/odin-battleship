dc3d82d546a28323978a3ee4ff78e24e4d6f527c
Aleks Michalak <24192100+aleksishere@users.noreply.github.com>
2024-06-24 00:01:14 +0200

    Playing vs computer is done

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ class Ship {
 class Gameboard {
   constructor(playerName) {
     this.playerName = playerName;
+    this.shipCount = 0;
     this.board = [];
     for (let i = 0; i < 10; i++) {
       let row = []
@@ -37,13 +38,16 @@ class Gameboard {
   }
 
   placeShip(ship, x, y, dir) {
+    if (this.shipCount >= 5) return false;
     if (dir === "vertical") {
       if (x + ship.length > 10) {
-        throw new Error("Out of bounds!");
+        console.error("Out of bounds!");
+        return false;
       }
       for (let i = 0; i < ship.length; i++) {
         if (this.board[y][x + i] !== null) {
-          throw new Error("Ship collides with another ship!")
+          console.error("Ship collides with another ship!");
+          return false;
         }
       }
       for (let i = 0; i < ship.length; i++) {
@@ -51,19 +55,24 @@ class Gameboard {
       }
     } else if (dir === "horizontal") {
       if (y + ship.length > 10) {
-        throw new Error("Out of bounds!");
+        console.error("Out of bounds!");
+        return false;
       }
       for (let i = 0; i < ship.length; i++) {
-        if (this.board[y+i][x] !== null) {
-          throw new Error("Ship collides with another ship!")
+        if (this.board[y + i][x] !== null) {
+          console.error("Ship collides with another ship!");
+          return false;
         }
       }
       for (let i = 0; i < ship.length; i++) {
         this.board[y+i][x] = ship;
       }
     } else {
-      throw new Error("Invalid direction!")
+      console.error("Invalid direction!");
+      return false;
     }
+    this.shipCount++;
+    return true;
   }
 
   receiveAttack(x, y) {
@@ -76,8 +85,8 @@ class Gameboard {
     } else {
       this.board[y][x] = "miss"
       result = { result: 'miss' };
+      changeTurn()
     }
-    changeTurn()
     return result;
   }
 
@@ -93,6 +102,21 @@ class Gameboard {
         }
       }
       return true;
+  }
+
+  generateBoard() {
+    if (isGameReady) {
+      console.error("Game already started!")
+      return false;
+    }
+    while (this.shipCount < 5) {
+      const direction = Math.random() > 0.5 ? 'horizontal' : 'vertical';
+      const row = Math.floor(Math.random() * 10);
+      const col = Math.floor(Math.random() * 10);
+      const length = Math.floor(Math.random() * (4 - 1)) + 1;
+      this.placeShip(new Ship(length), col, row, direction)
+    }
+    drawBoards()
   }
 
 }
@@ -133,6 +157,28 @@ class Guessboard {
     }
     return attackResult;
   }
+
+  guessRandom() {
+    if (!isGameReady) {
+      console.warn("Game is not ready!");
+      return false;
+    }
+
+    let x, y;
+    while (player2.turn) {
+      do {
+        x = Math.floor(Math.random() * this.board.length);
+        y = Math.floor(Math.random() * this.board.length);
+      } while (this.board[y][x] !== null);
+
+      const attackResult = this.opponent.receiveAttack(x, y);
+      if (attackResult.result === 'hit') {
+        this.board[y][x] = 'hit'; 
+      } else if (attackResult.result === 'miss') {
+        this.board[y][x] = 'miss';
+      } 
+    }
+  }
 }
 
 class Player {
@@ -151,30 +197,10 @@ class Player {
 
 }
 
-class RealPlayer extends Player {
-  constructor(name) {
-    super(name)
-  }
-}
-
-class BotPlayer extends Player {
-  constructor(name) {
-    super(name)
-  }
-}
-let isGameReady = false;
-const player1 = new RealPlayer();
-const player2 = new RealPlayer();
-player1.setName("Aleks");
-player2.setName("Player 2");
-player1.turn = true;
-player1.guessboard.setOpponent(player2.gameboard)
-player2.guessboard.setOpponent(player1.gameboard)
-drawBoards(player1, player2);
-
-function drawBoards(player1, player2) {
+function drawBoards() {
   boardsDiv.innerHTML = '';
-  const players = [player1, player2];
+  let players = []
+  isBot ? players = [player1] : players = [player1, player2]
   players.forEach(player => {
     const playerContainer = document.createElement('div');
     playerContainer.classList.add("player-container");
@@ -184,8 +210,8 @@ function drawBoards(player1, player2) {
     pNickname.classList.add("nickname");
     playerContainer.appendChild(pNickname);
 
-     const pGameboard = document.createElement('div');
-      pGameboard.classList.add("gameboard");
+    const pGameboard = document.createElement('div');
+    pGameboard.classList.add("gameboard");
       player.gameboard.board.forEach(row => {
       const rowDiv = document.createElement("div");
       rowDiv.classList.add("row");
@@ -220,12 +246,15 @@ function drawBoards(player1, player2) {
         cellDiv.style.cursor = "pointer";
         cellDiv.addEventListener('click', function (event) {
           if (player1.turn && event.target.parentNode.parentNode.id === player1.name) {
-            player1.guessboard.guess(columnIndex, rowIndex, player1)
+            player1.guessboard.guess(columnIndex, rowIndex)
+            if (isBot && player2.turn) {
+              player2.guessboard.guessRandom();
+            }
           }
           if (player2.turn && event.target.parentNode.parentNode.id === player2.name) {
-            player2.guessboard.guess(columnIndex, rowIndex, player2)
+            player2.guessboard.guess(columnIndex, rowIndex)
           }
-          drawBoards(player1, player2)
+          drawBoards()
           checkForWin();
         });
         if (cell === null) {
@@ -246,13 +275,19 @@ function drawBoards(player1, player2) {
 }
 
 function checkForWin() {
-  if (isGameReady) {
+  if (isGameReady && !isGameFinished) {
     if (player1.gameboard.checkIfPlayerWon()) {
+      isGameFinished = true;
       alert(`${player2.name} won!`);
     } else if (player2.gameboard.checkIfPlayerWon()) {
+      isGameFinished = true;
       alert(`${player1.name} won!`);
     }  
   }
+}
+
+function checkEqualShipCount() {
+  return player1.gameboard.shipCount === player2.gameboard.shipCount;
 }
 
 function changeTurn() {
@@ -267,15 +302,21 @@ restartBtn.addEventListener("click", function () {
   player1.guessboard.setOpponent(player2.gameboard)
   player2.guessboard.setOpponent(player1.gameboard)
   isGameReady = false;
+  isGameFinished = true;
+  if (isBot) player2.gameboard.generateBoard(); // Generate board when restarting board against bot.
 })
 
 startBtn.addEventListener("click", () => {
   if (isGameReady) {
     throw new Error("Game already started")
   };
+  if (!checkEqualShipCount()) {
+    throw new Error("Players ship count is not equal")
+  }
   player1.turn = true;
   player2.turn = false;
   isGameReady = true;
+  isGameFinished = false;
 })
 
 document.getElementById("player1Btn").addEventListener("click", () => {
@@ -292,3 +333,41 @@ document.getElementById("player2Btn").addEventListener("click", () => {
     drawBoards(player1, player2)
   }
 })
+
+let isBot = false;
+let isGameFinished = false;
+let isGameReady = false;
+const player1 = new Player();
+const player2 = new Player();
+player1.setName("Player 1");
+player2.setName("Player 2");
+player1.turn = true;
+player1.guessboard.setOpponent(player2.gameboard)
+player2.guessboard.setOpponent(player1.gameboard)
+drawBoards(player1, player2);
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("playerModal").style.display = "block";
+  document.getElementById("playerForm").onsubmit = function(e) {
+    e.preventDefault();
+    const player1Name = document.getElementById("player1Name").value;
+    const player2Name = document.getElementById("player2Name").value;
+    isBot = document.getElementById("player2IsBot").checked;
+    player1.setName(player1Name);
+    player1.setName(player2Name);
+    document.getElementById("player1Nickname").innerText = player1Name
+    document.getElementById("player2Nickname").innerText = player2Name
+    player1.guessboard.setOpponent(player2.gameboard)
+    player2.guessboard.setOpponent(player1.gameboard)
+    if (isBot) {
+      player2.gameboard.generateBoard();
+      document.getElementById('player2-control').style.display = "none";
+    }
+    drawBoards();
+
+    document.getElementById("playerModal").style.display = "none";
+  };
+});
+
+document.getElementById("player1GenerateBoard").addEventListener("click", () => {player1.gameboard.generateBoard()})
+document.getElementById("player2GenerateBoard").addEventListener("click", () => {player2.gameboard.generateBoard()})
